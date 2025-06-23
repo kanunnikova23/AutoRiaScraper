f3d4dcb21c64d1b0ec22f5046215047bd23c3ede
Oleksandra <138119282+kanunnikova23@users.noreply.github.com>
2025-06-23 13:26:53 +0100

    feat(main): add async DB initialization on app start

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+from app.db.session import async_engine
+from app.db.models import Base
+import asyncio
+
+
+# Створення таблиць у БД (якщо їх ще нема)
+async def init_db():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
 def main():
     print("AutoRiaScraper - перший запуск")
-
+    asyncio.run(init_db())  # асинхронна ініціалізація БД
 
 
 # Цей блок виконується тільки якщо файл запускається напряму
