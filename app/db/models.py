7494b73aebf6e08d1aff62ad0dbcfaa7b9d1625f
Oleksandra <138119282+kanunnikova23@users.noreply.github.com>
2025-06-24 11:48:07 +0100

    feat(models): update Car model to use UUID primary key and enforce URL uniqueness

--- app/db/models.py
+++ app/db/models.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, Date
 from app.db import Base
-from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
 # Модель для таблиці автомобілів, яка парситься з AutoRia
 class Car(Base):
     __tablename__ = "cars"
 
-    url = Column(String, primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    url = Column(String, unique=True, nullable=False)
     title = Column(String)
     price_usd = Column(Integer)
     odometer = Column(Integer)
@@ -15,6 +17,6 @@ class Car(Base):
     phone_number = Column(String)
     image_url = Column(String)
     images_count = Column(Integer)
-    car_number = Column(String)
-    car_vin = Column(String)
-    datetime_found = Column(DateTime, default=datetime.now())
+    car_number = Column(String, unique=False, nullable=True)
+    car_vin = Column(String, unique=False, nullable=True)
+    datetime_found = Column(Date, nullable=False)
